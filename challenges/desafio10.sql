c392d9f172d21514373398748ff186241ec259ba
nickolasvm <nickolas64vm@gmail.com>
2023-03-08 12:31:12 -0300

    feat: Normalizado tabela de canções favoritas para a 3ª Forma Normal

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,24 @@
--- Descomente e altere as linhas abaixo:
+CREATE TABLE SpotifyClone.favorite_musics 
+(
+    user_id	INT NOT NULL,
+    music_id	INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (music_id) REFERENCES musics (music_id),
+    CONSTRAINT PRIMARY KEY(user_id, music_id)
+) engine = InnoDB;
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO favorite_musics (user_id, music_id)
+VALUES
+	(1, 3),
+	(1, 6),
+	(1, 10),
+	(2, 4),
+	(3, 1),
+	(3, 3),
+	(4, 7),
+	(4, 4),
+	(5, 2),
+	(5, 10),
+	(8, 4),
+	(9, 7),
+	(10, 3);
